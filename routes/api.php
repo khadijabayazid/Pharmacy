d51bd692f32d8ba30604a397836c3d94a8a9b9c6
khadijabayazid <khokhaba2005@gmail.com>
2026-08-14 04:21:14 +0300

    feat(api): add category and product CRUD routes

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,14 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 Route::prefix('auth')->group(function(){
     Route::post('register',[AuthController::class, 'register']);
@@ -17,3 +19,16 @@ Route::prefix('auth')->group(function(){
         Route::get('me', [AuthController::class, 'me']);
     });
 });
+
+Route::get('categories', [CategoryController::class, 'index']);
+Route::get('categories/{category}', [CategoryController::class, 'show']);
+Route::post('categories', [CategoryController::class, 'store']);
+Route::put('categories/{category}', [CategoryController::class, 'update']);
+Route::delete('categories/{category}', [CategoryController::class, 'destroy']);
+
+Route::get('products/search', [ProductController::class, 'search']);
+Route::get('products', [ProductController::class, 'index']);
+Route::get('products/{product}', [ProductController::class, 'show']);
+Route::post('products', [ProductController::class, 'store']);
+Route::post('products/{product}', [ProductController::class, 'update']);
+Route::delete('products/{product}', [ProductController::class, 'destroy']);
