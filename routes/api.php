5a044b62260848b866110cd49b146fa1a98ee448
khadijabayazid <khokhaba2005@gmail.com>
2026-08-22 02:34:45 +0300

    chore(routes): update api routes for products, orders and auth restrictions

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,25 +11,38 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::prefix('auth')->group(function(){
-    Route::post('register',[AuthController::class, 'register']);
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
 
-    Route::middleware('auth:sanctum')->group(function(){
-        Route::post('logout',[AuthController::class, 'logout']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout']);
         Route::get('me', [AuthController::class, 'me']);
     });
 });
 
-Route::get('categories', [CategoryController::class, 'index']);
-Route::get('categories/{category}', [CategoryController::class, 'show']);
-Route::post('categories', [CategoryController::class, 'store']);
-Route::put('categories/{category}', [CategoryController::class, 'update']);
-Route::delete('categories/{category}', [CategoryController::class, 'destroy']);
+Route::middleware('auth:sanctum')->group(function () {
 
-Route::get('products/search', [ProductController::class, 'search']);
-Route::get('products', [ProductController::class, 'index']);
-Route::get('products/{product}', [ProductController::class, 'show']);
-Route::post('products', [ProductController::class, 'store']);
-Route::post('products/{product}', [ProductController::class, 'update']);
-Route::delete('products/{product}', [ProductController::class, 'destroy']);
+
+    Route::get('categories', [CategoryController::class, 'index']);
+    Route::get('categories/{category}', [CategoryController::class, 'show']);
+    Route::post('categories', [CategoryController::class, 'store']);
+    Route::put('categories/{category}', [CategoryController::class, 'update']);
+    Route::delete('categories/{category}', [CategoryController::class, 'destroy']);
+
+
+    Route::get('products/search', [ProductController::class, 'search']);
+    Route::get('products', [ProductController::class, 'index']);
+    Route::get('products/{product}', [ProductController::class, 'show']);
+    Route::post('products', [ProductController::class, 'store']);
+    Route::post('products/{product}', [ProductController::class, 'update']);
+    Route::delete('products/{product}', [ProductController::class, 'destroy']);
+
+    Route::post('orders', [OrderController::class, 'store']);
+    Route::get('orders', [OrderController::class, 'index']);
+    Route::get('orders/{order}', [OrderController::class, 'show']);
+    Route::post('orders/{order}/accept', [OrderController::class, 'accept']);
+    Route::post('orders/{order}/reject', [OrderController::class, 'reject']);
+    Route::post('orders/{order}/assign-delivery', [OrderController::class, 'assignDelivery']);
+    Route::post('orders/{order}/mark-delivered', [OrderController::class, 'markDelivered']);
+});
